94a299972232bbe49b3111cd702fdc2e9e37f25b
arunr <arunr@outlook.com>
2015-06-28 02:39:02 +0100

    Added angular app creation

--- www/agent/app.js
+++ www/agent/app.js
@@ -1,1 +1,23 @@
-var sabreHack = angular.module("sabreHack", []);
+var sabreHack = angular.module("sabreHack", [])
+    .controller('mainCtrl', function($scope, $http) {
+        $scope.submit = function() {
+            $http.post('/api/v1/login', $scope.user).then(function(data) {
+                $scope.logged_in = true;
+            }, function(err) {
+                $scope.logged_in = false;
+            });
+        };
+
+        $scope.saveapp = function() {
+            console.log("CALLED SAVEAPP");
+            $http.post('/api/v1/apps', $scope.app).then(function(data) {
+                $scope.saved = $scope.saved || {};
+                $scope.saved.status = true;
+                $scope.saved.data = data;
+            }, function(err) {
+                $scope.saved = $scope.saved || {};
+                $scope.saved.status = false;
+                $scope.saved.data = err;
+            });
+        }
+    });
